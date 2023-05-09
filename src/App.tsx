9bc75b7529ce205820743a7fffe489f24c07edb9
Bartosz Zagozdon <bart.zagozdon@gmail.com>
2023-05-09 18:32:21 +0200

    Stepper functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import styled from "styled-components";
-import { useSelector } from "react-redux";
+//import styled from 'styled-components';
+//import { useSelector } from 'react-redux';
 
-const Contener = styled.div`
-  font-size: 36px;
-`;
+import { FeelBetter } from "./components/FeelBetter";
+
+// const Contener = styled.div`
+//   font-size: 36px;
+// `;
 
 function App() {
-  const text: any = useSelector((state) => state);
+  //const text: any = useSelector((state) => state);
 
   return (
-    <Contener>
-      {text.exampleReducer.text}
-      <Routes>
-        <Route path={"/home"} element={<div>home</div>} />
-        <Route path={"/login"} element={<div>login</div>} />
-      </Routes>
-    </Contener>
+    //<Contener>
+    //{text.exampleReducer.text}
+    <Routes>
+      <Route path={"/home"} element={<div>home</div>} />
+      <Route path={"/login"} element={<div>login</div>} />
+      <Route path={"/feelbetter"} element={<FeelBetter />} />
+    </Routes>
+    //</Contener>
   );
 }
 
