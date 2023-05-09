fc676c7385df30bc3895b46cae06dc8168dfd6b5
Bartosz Zagozdon <bart.zagozdon@gmail.com>
2023-05-09 18:54:23 +0200

    Moved FeelBetter.tsx to routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 //import styled from 'styled-components';
 //import { useSelector } from 'react-redux';
 
-import { FeelBetter } from "./components/FeelBetter";
+import { FeelBetter } from "./components/routes/FeelBetter";
 
 // const Contener = styled.div`
 //   font-size: 36px;
