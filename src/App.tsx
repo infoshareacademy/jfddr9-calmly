29e71e5e4bb415de7a8796bf48c820ac1ac8a62f
Anna Bachorska <anakonceptinfo@gmail.com>
2023-05-11 10:39:41 +0200

    created Register and Login - in progress

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { Route, Routes } from "react-router-dom";
 //import styled from 'styled-components';
 //import { useSelector } from 'react-redux';
 
-import { FeelBetter } from "./components/routes/FeelBetter";
+import { FeelBetter } from "./routes/FeelBetter";
+import { Register } from "./auth/Register";
+import { Login } from "./auth/Login";
 
 // const Contener = styled.div`
 //   font-size: 36px;
@@ -15,9 +17,11 @@ function App() {
   return (
     //<Contener>
     //{text.exampleReducer.text}
+
     <Routes>
       <Route path={"/home"} element={<div>home</div>} />
-      <Route path={"/login"} element={<div>login</div>} />
+      <Route path={"/register"} element={<Register />} />
+      <Route path={"/login"} element={<Login />} />
       <Route path={"/feelbetter"} element={<FeelBetter />} />
     </Routes>
     //</Contener>
