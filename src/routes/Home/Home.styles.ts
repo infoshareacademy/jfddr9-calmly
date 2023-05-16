b218cf6a3c58d5192932e5e9d7ee43bbda7f3c9d
Nicoletta Czarnacka <nika.czarnacka@gmail.com>
2023-05-16 21:17:31 +0200

    layout changed

--- src/routes/Home/Home.styles.ts
+++ src/routes/Home/Home.styles.ts
@@ -1,69 +1,123 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  background-color: ;
+  background-image: url("src/assets/g page.png");
   background-size: auto 100%;
   background-repeat: no-repeat;
   background-position: right;
   width: 100vw;
   height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
   font-family: "Outfit", sans-serif;
 `;
 
 export const LogoWhite = styled.img`
+  position: absolute;
   width: 250px;
   height: 78.95px;
-  left: 140px;
+  left: 100px;
   top: 116px;
 `;
 
-export const Nav = styled.a`
-  width: 331px;
-  height: 30px;
-  left: 922px;
-  top: 100px;
+export const Nav = styled.nav`
+  width: 100%;
+  height: 50px;
   font-family: "Outfit";
   font-style: normal;
   font-size: 12px;
   line-height: 30px;
-  display: flex;
-  margin-left: 500px;
   align-items: center;
   text-align: center;
   color: #ffffff;
+  display: flex;
+  gap: 40px;
+
+  & a {
+    font-size: 20px;
+    font-weight: 700;
+  }
 `;
 
 export const WrapperContent = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 500px;
-  margin-left: 110px;
-  color: #797bec;
+  width: 600px;
+  position: absolute;
+  top: 130px;
+  right: 250px;
+  color: white;
+`;
+
+export const WrapperText = styled.div`
+  width: 400px;
+  align-self: flex-start;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 `;
 
 export const WrapperButtons = styled.div`
+  width: 100%;
   display: flex;
   gap: 30px;
 `;
 
-export const TextUnderLogo = styled.div`
-  margin-bottom: 100px;
-  margin-top: 100px;
+export const Header = styled.div`
+  margin-top: 60px;
+  font-size: 40px;
+  font-weight: 600;
+  & span {
+    font-weight: 700;
+  }
+`;
+
+export const TextUnderHeader = styled.div`
+  font-size: 30px;
+  margin-top: 10px;
+  color: rgba(255, 255, 255, 0.7);
+
+  & span {
+    color: rgb(255, 255, 255);
+  }
 `;
 
 export const Paragraph = styled.p`
-  margin-bottom: 50px;
-  padding-inline: 40px;
+  margin-bottom: 40px;
+  padding-right: 70px;
+  text-align: justify;
+`;
+
+export const ButtonLogOut = styled.button`
+  cursor: pointer;
+  border: none;
+  width: 105px;
+  height: 34px;
+  border-radius: 50px;
+  background: rgba(255, 255, 255, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: 700;
+
+  &:hover {
+    transition: 0.3s ease-in;
+    opacity: 0.9;
+    transform: scale(103%);
+  }
+
+  &:active {
+    transition: 0.1s ease-in;
+    transform: scale(99%);
+  }
 `;
 
 export const Button = styled.button`
+  border: none;
   cursor: pointer;
-  width: 180px;
-  height: 56px;
+  width: 150px;
+  height: 36px;
   border-radius: 50px;
   background: #797bec;
   display: flex;
--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -4,29 +4,32 @@ import * as Styled from "./Home.styles";
 export const Home = () => {
   return (
     <Styled.Wrapper>
+      <Styled.LogoWhite src="src/assets/logo-white.png" />
       <Styled.WrapperContent>
         <Styled.Nav>
-          <Styled.LogoWhite src="src/assets/logo-white.png" />
-          <ul>
-            <li>Let's feel better</li>
-            <li>Support</li>
-            <li>About us</li>
-            <li>
-              <Styled.Button>Log out</Styled.Button>
-            </li>
-          </ul>
+          <a>Let's feel better</a>
+          <a>Support</a>
+          <a>About us</a>
+          <Styled.ButtonLogOut>Log out</Styled.ButtonLogOut>
         </Styled.Nav>
-        <Styled.TextUnderLogo>Let your stress drop</Styled.TextUnderLogo>
-        <Styled.Paragraph>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi,
-          facilis officia velit facere libero reprehenderit! Perspiciatis saepe
-          illum repellat labore? Deleniti atque ex qui? Consequatur obcaecati
-          repellat quod tempore perspiciatis.
-        </Styled.Paragraph>
-        <Styled.WrapperButtons>
-          <Styled.Button>START</Styled.Button>
-          <Styled.Button>JOURNAL</Styled.Button>
-        </Styled.WrapperButtons>
+        <Styled.WrapperText>
+          <Styled.Header>
+            Hi, <span>Janusz!</span>
+          </Styled.Header>
+          <Styled.TextUnderHeader>
+            Let your <span>stress</span> drop
+          </Styled.TextUnderHeader>
+          <Styled.Paragraph>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi,
+            facilis officia velit facere libero reprehenderit! Perspiciatis
+            saepe illum repellat labore? Deleniti atque ex qui? Consequatur
+            obcaecati repellat quod tempore perspiciatis.
+          </Styled.Paragraph>
+          <Styled.WrapperButtons>
+            <Styled.Button>START</Styled.Button>
+            <Styled.Button>JOURNAL</Styled.Button>
+          </Styled.WrapperButtons>
+        </Styled.WrapperText>
       </Styled.WrapperContent>
     </Styled.Wrapper>
   );
