cdc3f8f7ce255c6edb19a48cb275681b122690ae
Bartosz Zagozdon <bart.zagozdon@gmail.com>
2023-05-18 11:56:04 +0200

    Journal in progress

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,15 +1,16 @@
-import { useDispatch } from "react-redux";
-// import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import * as Styled from "./Home.styles";
 import { useEffect } from "react";
 import { updateBg } from "../../store/slice";
-//import { auth } from "../../api/firebase";//
-//import { signOut } from "firebase/auth"; //
+import { auth } from "../../api/firebase"; //
+import { signOut } from "firebase/auth"; //
 //import { Footer } from "../footer/Footer"; //
 
 export const Home = () => {
   const dispatch = useDispatch();
-  //const navigate = useNavigate();//
+  const navigate = useNavigate();
+  const { authUser }: any = useSelector((state) => state);
 
   useEffect(() => {
     dispatch(updateBg("bgHome"));
@@ -17,33 +18,41 @@ export const Home = () => {
 
   return (
     <Styled.Wrapper>
-      <Styled.LogoWhite src="src/assets/logo-white.png" />
-      <Styled.WrapperContent>
-        <Styled.Nav>
-          <a>Let's feel better</a>
-          <a>Support</a>
-          <a>About us</a>
-          <Styled.ButtonLogOut>Log out</Styled.ButtonLogOut>
-        </Styled.Nav>
-        <Styled.WrapperText>
-          <Styled.Header>
-            Hi, <span>Janusz!</span>
-          </Styled.Header>
-          <Styled.TextUnderHeader>
-            Let your <span>stress</span> drop
-          </Styled.TextUnderHeader>
-          <Styled.Paragraph>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi,
-            facilis officia velit facere libero reprehenderit! Perspiciatis
-            saepe illum repellat labore? Deleniti atque ex qui? Consequatur
-            obcaecati repellat quod tempore perspiciatis.
-          </Styled.Paragraph>
-          <Styled.WrapperButtons>
-            <Styled.Button>START</Styled.Button>
-            <Styled.Button>JOURNAL</Styled.Button>
-          </Styled.WrapperButtons>
-        </Styled.WrapperText>
-      </Styled.WrapperContent>
+      <>
+        <Styled.LogoWhite src="src/assets/logo-white.png" />
+        <Styled.WrapperContent>
+          <Styled.Nav>
+            <a onClick={() => navigate("/feelbetter")}>Let's feel better</a>
+            <a onClick={() => navigate("/support")}>Support</a>
+            <a onClick={() => navigate("/about")}>About us</a>
+            <Styled.ButtonLogOut onClick={() => signOut(auth)}>
+              Log out
+            </Styled.ButtonLogOut>
+          </Styled.Nav>
+          <Styled.WrapperText>
+            <Styled.Header>
+              Hi, <span>{authUser.fullName}!</span>
+            </Styled.Header>
+            <Styled.TextUnderHeader>
+              Let your <span>stress</span> drop
+            </Styled.TextUnderHeader>
+            <Styled.Paragraph>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi,
+              facilis officia velit facere libero reprehenderit! Perspiciatis
+              saepe illum repellat labore? Deleniti atque ex qui? Consequatur
+              obcaecati repellat quod tempore perspiciatis.
+            </Styled.Paragraph>
+            <Styled.WrapperButtons>
+              <Styled.Button onClick={() => navigate("/quiz")}>
+                START
+              </Styled.Button>
+              <Styled.Button onClick={() => navigate("/journal")}>
+                JOURNAL
+              </Styled.Button>
+            </Styled.WrapperButtons>
+          </Styled.WrapperText>
+        </Styled.WrapperContent>
+      </>
     </Styled.Wrapper>
   );
 };
