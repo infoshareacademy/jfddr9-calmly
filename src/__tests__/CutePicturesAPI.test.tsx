671d5b163244a6aad2cadcdc28146aca93c47e3b
piechotadawid2000@gmail.com <piechotadawid2000@gmail.com>
2023-05-24 12:57:28 +0200

    set up for tests

---
+++ src/__tests__/CutePicturesAPI.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import { CutePictures } from "../api/CutePicturesAPI";
+
+test("Should render cute pictures", () => {
+  //   it("should render", () => {
+  // given
+  render(<CutePictures />);
+
+  // then
+  // expect(screen.getByRole("heading")).toHaveTextContent(/Doggy Directory/);
+  expect(screen.getByRole("StyledNewPicButton")).toHaveDisplayValue(
+    "generate new picture"
+  );
+  //   });
+});
