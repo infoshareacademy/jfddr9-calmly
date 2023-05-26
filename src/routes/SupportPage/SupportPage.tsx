fa58f190aef55134e2e09ac4b806869c374853d3
piechotadawid2000@gmail.com <piechotadawid2000@gmail.com>
2023-05-26 11:21:11 +0200

    en to UK

--- src/routes/SupportPage/SupportPage.tsx
+++ src/routes/SupportPage/SupportPage.tsx
@@ -20,7 +20,7 @@ export const SupportPage = () => {
           <h2>Are you in a crisis?</h2>
           <a>If you need help or psychological support, please call:</a>
           <span>PL +48 800 70 2222</span>
-          <span>EN 116 123</span>
+          <span>UK 116 123</span>
           <h3>free 24/7 support at your fingertips</h3>
         </Styled.WrapperLeft>
         <Styled.WrapperRight>
